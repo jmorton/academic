81e10965980d8e55a1c0c35f7542006297385cb6
Jon Morton <jon@jonmorton.com>
2008-09-04 11:34:01 -0400

    Homework 1 - changed to floating point type for averaging.

--- cs367/homework-01/average.c
+++ cs367/homework-01/average.c
@@ -1,5 +1,8 @@
 /*   author: Jon Morton -- jon.morton@gmail.com
+ * 
  *    about: Homework 1 for CS367 at GMU
+ *           http://www.cs.gmu.edu/~rcarver/cs367/hm1.txt
+ *
  *  created: 28-August 2008
  *
  *  This program solves the problem and meets all special requirements.
@@ -11,7 +14,7 @@
 #define MAXINPUT 20
 
 // Calculate average of a set of numbers
-int average(int *numbers, int size) {
+float average(int *numbers, int size) {
   int ix = 0;
   int sum = 0;
   
@@ -23,24 +26,23 @@ int average(int *numbers, int size) {
 }
 
 int main(void) {
+  // Create placeholder for user inputs
+  int inputs[MAXINPUT];
   int inputLimit = 0;
+  int ix = 0;
   
   // Figure out how many numbers the user will enter
   printf("How many numbers?: ");
   scanf("%d", &inputLimit);
   printf("Enter the numbers. (Press <Enter> after each number.)\n");
   
-  // Create placeholder for user inputs
-  int inputs[inputLimit];
-  
   // Gather user input (without error handling)
-  int ix = 0;
   for(ix = 0; ix < inputLimit; ix++) {
     scanf("%d", &inputs[ix]);
   }
   
   // Calculate and display average of person's inputs
-  printf("The average is %d\n", average(inputs, inputLimit));
+  printf("The average is %.1f\n", average(inputs, inputLimit));
   
   return EXIT_SUCCESS;
 }
