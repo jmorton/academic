ad12d532d5b37ca9a5d60e778425e2bfcbbd624b
Jon Morton <jmorton@youtopia.local>
2009-08-05 21:46:37 -0400

    subst* works with nested lists

--- scheme/subst*.scm
+++ scheme/subst*.scm
@@ -2,8 +2,18 @@
 	(lambda (new old l)
 		(cond
 			((null? l) '())
-			((eq? (car l) old)
-				(cons new (subst* new old (cdr l))))
-			(else
-				(cons (car l) (subst* new old (cdr l))))
+			((atom? (car l))
+				(cond
+					((eq? (car l) old)
+						(cons
+							new
+							(subst* new old (cdr l))))
+				(else
+					(cons
+						(car l)
+						(subst* new old (cdr l))))))
+			(else ; it's a list
+				(cons
+					(subst* new old (car l))
+					(subst* new old (cdr l))))
 )))
