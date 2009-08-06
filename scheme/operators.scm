c5928c79e0554d84a3e18e155dfbd218c0ac999b
Jon Morton <jmorton@youtopia.local>
2009-08-05 21:20:54 -0400

    defined less than and equal (for numbers)

--- scheme/operators.scm
+++ scheme/operators.scm
@@ -3,7 +3,23 @@
 (define >
 (lambda (n m)
 (cond
-((zero? n) #f)
-((zero? m) #t)
-(else (> (sub1 n) (sub1 m)))
+ ((zero? n) #f)
+ ((zero? m) #t)
+ (else (> (sub1 n) (sub1 m)))
+)))
+
+(define <
+	(lambda (n m)
+		(cond
+			((zero? m) #f)
+			((zero? n) #t)
+			(else (< (sub1 n) (sub1 m)))
+)))
+
+(define =
+	(lambda (n m)
+		(cond
+			((> n m) #f)
+			((< n m) #f)
+			(else #t)
 )))
