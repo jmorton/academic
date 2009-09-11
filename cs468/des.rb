c2494a302686ef944bbc822257b720e7bb9020ef
Jon Morton <jmorton@youtopia.local>
2009-09-11 19:55:53 -0400

    Implemented substitution function.

--- cs468/des.rb
+++ cs468/des.rb
@@ -1,10 +1,35 @@
 module DES
   
+  S1 = %w(1 0 3 2 3 2 1 0 0 2 1 3 3 1 3 2)
+  S2 = %w(0 1 2 3 2 0 1 3 3 0 1 0 2 1 0 3)
+  
   module Utility
   
+    def self.dec_to_bin(x)
+      x.to_i.to_s(2)
+    end
+    
+    def self.bin_to_dec(x)
+      x.to_i(2)
+    end
+    
     def self.permute(mapping, values)
       raise "Sizes of mapping and values differ when they shouldn't." if mapping.length != values.length
       mapping.collect { |index| values[index.to_i - 1] }
+    end
+    
+    def self.expand(mapping, values)
+      mapping.collect { |index| values[index.to_i - 1] }
+    end
+    
+    def self.substitute(input, mapping, size=4, &block)
+      row = DES::Utility.bin_to_dec(input[0].chr + input[3].chr).to_i
+      col = DES::Utility.bin_to_dec(input[1].chr + input[2].chr).to_i
+      if block_given?
+        yield(row,col)
+      else
+        mapping[(row*size + col).to_i].to_i
+      end
     end
     
   end
--- cs468/des_spec.rb
+++ cs468/des_spec.rb
@@ -1,5 +1,18 @@
 require 'des'
 
+describe 'Binary/Decimal conversion' do
+  
+  it 'should convert decimal to binary' do
+    DES::Utility.bin_to_dec("0").should eql(0)
+    DES::Utility.bin_to_dec("1").should eql(1)
+    DES::Utility.bin_to_dec("10").should eql(2)
+  end
+  
+  it 'should convert binary to decimal' do
+    DES::Utility.dec_to_bin(0).should eql("0")
+  end
+end
+
 describe 'Permutation' do
   
   it 'should support permutation' do
@@ -24,3 +37,59 @@ describe 'Permutation' do
   
 end
 
+describe 'Expansion' do
+  it 'should support expansion' do
+    DES::Utility.should respond_to(:expand)
+  end
+  
+  it 'should perform basic expansion' do
+    table  = %w(4 1 2 3 4 1)
+    result = %w(d a b c d a)
+    values = %w(a b c d)
+    
+    DES::Utility.expand(table, values).should eql(result)
+  end
+end
+
+describe 'Substitution' do
+  it 'should support substitution' do
+    DES::Utility.should respond_to(:substitute)
+  end
+  
+  it 'should perform basic substitution' do
+    s1 = DES::S1
+    s2 = DES::S2
+    
+    DES::Utility.substitute("0000",s1).should eql(1)
+    DES::Utility.substitute("0001",s1).should eql(3)
+    DES::Utility.substitute("1111",s1).should eql(2)
+    
+    DES::Utility.substitute("0000",s2).should eql(0)
+    DES::Utility.substitute("0001",s2).should eql(2)
+    DES::Utility.substitute("1111",s2).should eql(3)
+  end
+  
+  it 'should calculate the row using the first and last char' do
+    # Testing the row/col values that should be generated and
+    # not the values that are in individual mappings.
+    s1 = DES::S1
+    m1 = Proc.new { |row, column| [row, column] }
+    
+    DES::Utility.substitute("0000",s1,&m1).should eql([0,0])
+    DES::Utility.substitute("0001",s1,&m1).should eql([1,0])
+    DES::Utility.substitute("0010",s1,&m1).should eql([0,1])
+    DES::Utility.substitute("0011",s1,&m1).should eql([1,1])
+    DES::Utility.substitute("0100",s1,&m1).should eql([0,2])
+    DES::Utility.substitute("0101",s1,&m1).should eql([1,2])
+    DES::Utility.substitute("0110",s1,&m1).should eql([0,3])
+    DES::Utility.substitute("1000",s1,&m1).should eql([2,0])
+    DES::Utility.substitute("1001",s1,&m1).should eql([3,0])
+    DES::Utility.substitute("1010",s1,&m1).should eql([2,1])
+    DES::Utility.substitute("1011",s1,&m1).should eql([3,1])
+    DES::Utility.substitute("1100",s1,&m1).should eql([2,2])
+    DES::Utility.substitute("1101",s1,&m1).should eql([3,2])
+    DES::Utility.substitute("1110",s1,&m1).should eql([2,3])
+    DES::Utility.substitute("1111",s1,&m1).should eql([3,3])
+  end
+ 
+end
