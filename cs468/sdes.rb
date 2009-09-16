59e2b7141ae1137368734a0d99304f30417dae98
Jon Morton <jmorton@youtopia.local>
2009-09-15 21:55:39 -0400

    Implemented key schedule module and tests

---
+++ cs468/sdes.rb
@@ -0,0 +1,129 @@
+
+module SDES
+  
+  Initial = %w(2 6 3 1 4 8 5 7)
+  
+  Final = %w(4 1 3 5 7 2 8 6)
+  
+  EP = %w(4 1 2 3 2 3 4 1)
+  
+  P4 = %w(2 4 3 1)
+  
+  Flip = %w(5 6 7 8 1 2 3 4)
+  
+  S1 = %w(1 0 3 2
+          3 2 1 0
+          0 2 1 3
+          3 1 3 2)
+          
+  S2 = %w(0 1 2 3
+          2 0 1 3
+          3 0 1 0
+          2 1 0 3)
+          
+  P10 = %w(3 5 2 7 4 10 1 9 8 6)
+  
+  P8 = %w(6 3 7 4 8 5 10 9)
+  
+  module Key
+    
+    def self.subkey(key)
+      permuted_key = _p10(key)
+      
+      k1 = _p8( _ls1(_hi(permuted_key)) + _ls1(_lo(permuted_key)))
+      k2 = _p8( _ls3(_hi(permuted_key)) + _ls3(_lo(permuted_key)))
+      
+      [k1, k2]
+    end
+    
+    def self._p8(k)
+      a = k.each_char.map { |c| c }
+      Utility::permute(SDES::P8, a).to_s
+    end
+    
+    def self._p10(k)
+      a = k.each_char.map { |c| c }
+      Utility::permute(SDES::P10, a).to_s
+    end
+    
+    def self._hi(k)
+      k[0..4].to_s
+    end
+    
+    def self._lo(k)
+      k[5..9].to_s
+    end
+    
+    def self._ls1(a)
+      v = Utility::bin_to_dec(a)
+      Utility::rotate_left(v,1,5).bin(5)
+    end
+    
+    def self._ls3(a)
+      v = Utility::bin_to_dec(a)
+      Utility::rotate_left(v,3,5).bin(5)
+    end
+    
+  end
+  
+  module Utility
+    
+    def self.dec_to_bin(x, padding = 0)
+      binary = x.to_i.to_s(2)
+      (padding > 0) ? (binary.rjust(padding, "0")) : binary
+    end
+    
+    def self.bin_to_dec(x)
+      x.to_i(2)
+    end
+    
+    def self.rotate_left(x, shift, max_size = 8)
+      max_value = 2**max_size -1
+      ((x<<shift) | (x >> (max_size -shift))) & max_value
+    end
+    
+    def self.rotate_right(x, shift, max_size = 7)
+      shift &= max_size
+      max_value = (2 ** (max_size +1)) -1
+      ((x << (max_size +1 -shift)) | (x>>shift)) & max_value
+    end
+    
+    def self.mangle(input, key)
+      left = input[0..3]
+      right = input[4..7]
+      
+      left ^ (expand(SDES::EP, right) ^ key)
+    end
+  
+    def self.flip(a)
+      self.permute(SDES::Flip, a)
+    end
+     
+    def self.permute(mapping, values)
+      # raise "Sizes of mapping (#{mapping.length}) and values (#{values.length}) differ when they shouldn't." if mapping.length != values.length
+      mapping.collect { |index| values[index.to_i - 1] }
+    end
+    
+    def self.expand(mapping, values)
+      mapping.collect { |index| values[index.to_i - 1] }
+    end
+    
+    def self.substitute(input, mapping, size=4, &block)
+      row = SDES::Utility.bin_to_dec(input[0].chr + input[3].chr).to_i
+      col = SDES::Utility.bin_to_dec(input[1].chr + input[2].chr).to_i
+      if block_given?
+        yield(row,col)
+      else
+        mapping[(row*size + col).to_i].to_i
+      end
+    end
+    
+  end
+  
+end
+
+class Fixnum
+  def bin(padding)
+    SDES::Utility.dec_to_bin(self, padding)
+  end
+end
---
+++ cs468/sdes_spec.rb
@@ -0,0 +1,171 @@
+require 'sdes'
+
+describe 'Binary/Decimal conversion' do
+  
+  it 'should convert decimal to binary' do
+    SDES::Utility.bin_to_dec("0").should eql(0)
+    SDES::Utility.bin_to_dec("1").should eql(1)
+    SDES::Utility.bin_to_dec("10").should eql(2)
+  end
+  
+  it 'should convert binary to decimal' do
+    0.bin(4).should eql("0000")
+    1.bin(4).should eql("0001")
+    2.bin(4).should eql("0010")
+    4.bin(4).should eql("0100")
+    8.bin(4).should eql("1000")
+  end
+end
+
+describe 'Flip' do
+  
+  it 'Basic flipping works' do
+    SDES::Utility.flip([1,2,3,4,5,6,7,8]).should eql([5,6,7,8,1,2,3,4])
+  end
+  
+end
+
+describe 'Permutation' do
+  
+  it 'should support permutation' do
+    SDES::Utility.should respond_to(:permute)
+  end
+  
+  it 'should perform basic permutation' do
+    map = %w(3 2 1)
+    values = %w(a b c)
+    
+    SDES::Utility.permute(map, values).should eql(values.reverse)
+  end
+  
+  it 'should work when applying S-SDES initial and final permutation' do
+    before_p =%w(a b c d e f g h)
+    after_p  =%w(b f c a d h e g)
+    SDES::Utility.permute(SDES::Initial, before_p).should eql(after_p)
+    SDES::Utility.permute(SDES::Final, after_p).should eql(before_p)
+  end
+  
+end
+
+describe 'Expansion' do
+  it 'should support expansion' do
+    SDES::Utility.should respond_to(:expand)
+  end
+  
+  it 'should perform basic expansion' do
+    table  = %w(4 1 2 3 4 1)
+    result = %w(d a b c d a)
+    values = %w(a b c d)
+    
+    SDES::Utility.expand(table, values).should eql(result)
+  end
+end
+
+describe 'Substitution' do
+  it 'should support substitution' do
+    SDES::Utility.should respond_to(:substitute)
+  end
+  
+  it 'should perform basic substitution' do
+    s1 = SDES::S1
+    s2 = SDES::S2
+    
+    SDES::Utility.substitute("0000",s1).should eql(1)
+    SDES::Utility.substitute("0001",s1).should eql(3)
+    SDES::Utility.substitute("1111",s1).should eql(2)
+    
+    SDES::Utility.substitute("0000",s2).should eql(0)
+    SDES::Utility.substitute("0001",s2).should eql(2)
+    SDES::Utility.substitute("1111",s2).should eql(3)
+  end
+  
+  it 'should calculate the row using the first and last char' do
+    # Testing the row/col values that should be generated and
+    # not the values that are in individual mappings.
+    s1 = SDES::S1
+    m1 = Proc.new { |row, column| [row, column] }
+    
+    SDES::Utility.substitute("0000",s1,&m1).should eql([0,0])
+    SDES::Utility.substitute("0001",s1,&m1).should eql([1,0])
+    SDES::Utility.substitute("0010",s1,&m1).should eql([0,1])
+    SDES::Utility.substitute("0011",s1,&m1).should eql([1,1])
+    SDES::Utility.substitute("0100",s1,&m1).should eql([0,2])
+    SDES::Utility.substitute("0101",s1,&m1).should eql([1,2])
+    SDES::Utility.substitute("0110",s1,&m1).should eql([0,3])
+    SDES::Utility.substitute("1000",s1,&m1).should eql([2,0])
+    SDES::Utility.substitute("1001",s1,&m1).should eql([3,0])
+    SDES::Utility.substitute("1010",s1,&m1).should eql([2,1])
+    SDES::Utility.substitute("1011",s1,&m1).should eql([3,1])
+    SDES::Utility.substitute("1100",s1,&m1).should eql([2,2])
+    SDES::Utility.substitute("1101",s1,&m1).should eql([3,2])
+    SDES::Utility.substitute("1110",s1,&m1).should eql([2,3])
+    SDES::Utility.substitute("1111",s1,&m1).should eql([3,3])
+  end
+ 
+end
+
+describe 'Circular shifting' do
+  
+  it 'should rotate left correctly (with value of 1)' do
+    SDES::Utility.rotate_left(1,1).should eql(2)
+    SDES::Utility.rotate_left(1,2).should eql(4)
+    SDES::Utility.rotate_left(1,3).should eql(8)
+    SDES::Utility.rotate_left(1,4).should eql(16)
+    SDES::Utility.rotate_left(1,5).should eql(32)
+    SDES::Utility.rotate_left(1,6).should eql(64)
+    SDES::Utility.rotate_left(1,7).should eql(128)
+    SDES::Utility.rotate_left(1,8).should eql(1)
+  end
+  
+  it 'should rotate left correctly' do
+    SDES::Utility.rotate_left(255, 1).should eql(255)
+    SDES::Utility.rotate_left(127, 1).should eql(255-1)
+    SDES::Utility.rotate_left(63,  2).should eql(255-3)
+  end
+  
+  it 'should rotate right correctly' do
+    SDES::Utility.rotate_right(2,1).should eql(1)
+    SDES::Utility.rotate_right(255,1).should eql(255)
+  end
+  
+  it 'should rotate a five bit number correctly' do
+    x = SDES::Utility.bin_to_dec("1100")
+    y = SDES::Utility.rotate_left(x,2,4)
+    z = SDES::Utility.dec_to_bin(y)
+    z.should eql("11")
+  end
+  
+end
+
+describe 'Generating one key from another' do
+  it 'should generate a pair' do
+    keys = SDES::Key.subkey("1010000010")
+    keys[0].should eql("10100100")
+    keys[1].should eql("01000011")
+  end
+  
+  it 'should permute using P10 correctly' do
+    SDES::Key._p10("1010000010").should eql("1000001100")
+  end
+  
+  it 'should split correctly' do
+    SDES::Key._hi("1000001100").should eql("10000")
+    SDES::Key._lo("1000001100").should eql("01100")
+  end
+  
+  it 'should left shift by 1 correctly' do
+    SDES::Key._ls1("10000").should eql("00001")
+    SDES::Key._ls1("01100").should eql("11000")
+  end
+  
+  it 'should left shift by 3 correctly' do
+    SDES::Key._ls3("10000").should eql("00100")
+    SDES::Key._ls3("01100").should eql("00011")
+  end
+  
+  it 'should permute using P8 correctly' do
+    SDES::Key._p8("0000111000").should eql("10100100")
+    SDES::Key._p8("0010000011").should eql("01000011")
+  end
+  
+end
