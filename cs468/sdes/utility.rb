dd080655cc1a571617b98f71423953a012b2c48f
Jon Morton <jmorton@youtopia.local>
2009-09-23 01:22:32 -0400

    Simplified f-function.

--- cs468/sdes/utility.rb
+++ cs468/sdes/utility.rb
@@ -1,47 +1,6 @@
 module SDES
   module Utility
   
-    def self.decimal_to_binary(x, padding = 0)
-      binary = x.to_i.to_s(2)
-      (padding > 0) ? (binary.rjust(padding, "0")) : binary
-    end
-
-    def self.binary_to_decimal(x)
-      x.to_i(2)
-    end
-
-    def self.rotate_left(x, shift, max_size = 8)
-      max_value = 2**max_size -1
-      ((x<<shift) | (x >> (max_size -shift))) & max_value
-    end
-
-    def self.rotate_right(x, shift, max_size = 7)
-      shift &= max_size
-      max_value = (2 ** (max_size +1)) -1
-      ((x << (max_size +1 -shift)) | (x>>shift)) & max_value
-    end
-
-    # Review this, ensure that it "rotates" the output
-    # No tests written for this yet.
-    def self.fk(bits,sk)
-      left, right = bits[0..3], bits[4..7]
-      mangled = (left.base10 ^ f(right,sk).base10).bits(4)
-      mangled + right
-    end
-
-    # [48, 48, 49, 48, 49] => ['0', '0', '1', '0', '1'] => '00101'
-    def self.f(bits, subkey)
-      e = expand(SDES::EP, bits).base10
-      r = (e ^ subkey.base10).bits
-      r1, r2 = r[0..3], r[4..7]
-  
-      s0 = select_from(SDES::S0, r1)
-      s1 = select_from(SDES::S1, r2)
-      pr = (s0+s1).base10.bits(4)
-  
-      permute(P4,pr)
-    end
-
     # input: a string of ascii characters
     # output: a binary number as a string
     def self.encrypt(input, key)
@@ -70,16 +29,30 @@ module SDES
       chars.join
     end
 
-    def self.select_from(map, bits)
-      row = (bits[0].chr + bits[3].chr).base10
-      col = (bits[1].chr + bits[2].chr).base10
-      map[row*4+col].to_i.bits(2)
+    # Review this, ensure that it "rotates" the output.
+    # No tests written for this *yet* because it is used
+    # by f(), encrypt(), and decrypt() all of which
+    # have tests. Still, it deserves coverage.
+    def self.fk(bits,key)
+      left, right = bits[0..3], bits[4..7]
+      mangled = (left.base10 ^ f(right,key).base10).bits(4)
+      mangled + right
     end
+
+    # The nitty gritty mangler used by fk().  Even though the
+    # code can be inspected, what it does is not immediately
+    # obvious.
+    def self.f(bits, subkey)
+      mix = (subkey.base10 ^ expand(SDES::EP, bits).base10).bits
+      
+      sub_0 = select_two_bits_from(SDES::S0, mix[0..3])
+      sub_1 = select_two_bits_from(SDES::S1, mix[4..7])
+      
+      four_bits = (sub_0 + sub_1).base10.bits(4)
   
-    def self.flip(a)
-      permute(SDES::Flip, a)
+      permute(P4, four_bits)
     end
- 
+
     def self.permute(mapping, values)
       mapping.collect { |index| values[index.to_i - 1] }.as_str
     end
@@ -96,6 +69,36 @@ module SDES
       else
         mapping[(row*size + col).to_i].to_i
       end
+    end
+
+    def self.select_two_bits_from(map, bits)
+      row = (bits[0].chr + bits[3].chr).base10
+      col = (bits[1].chr + bits[2].chr).base10
+      map[row*4+col].to_i.bits(2)
+    end
+  
+    def self.flip(a)
+      permute(SDES::Flip, a)
+    end
+ 
+    def self.decimal_to_binary(x, padding = 0)
+      binary = x.to_i.to_s(2)
+      (padding > 0) ? (binary.rjust(padding, "0")) : binary
+    end
+
+    def self.binary_to_decimal(x)
+      x.to_i(2)
+    end
+
+    def self.rotate_left(x, shift, max_size = 8)
+      max_value = 2**max_size -1
+      ((x<<shift) | (x >> (max_size -shift))) & max_value
+    end
+
+    def self.rotate_right(x, shift, max_size = 7)
+      shift &= max_size
+      max_value = (2 ** (max_size +1)) -1
+      ((x << (max_size +1 -shift)) | (x>>shift)) & max_value
     end
 
   end
