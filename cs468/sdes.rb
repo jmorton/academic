a91b030685d38b35f94825f1a0860aec3407b52f
Jon Morton <jmorton@youtopia.local>
2009-09-22 22:51:04 -0400

    Added comments.

--- cs468/sdes.rb
+++ cs468/sdes.rb
@@ -1,4 +1,25 @@
+#
+# author: Jon Morton
+#
+# = About
+# A simplified and less secure version of DES.  This was implemented
+# as part of my coursework for CS 468 at George Mason University. If
+# you use this code commercially, you're making a huge mistake.
+#
+# = Important Notes
+# 1. I did not take an object oriented approach to implementing the majority
+# of the cryptographic functions.
+# 2. I've "monkey-patched" String, Fixnum, and Array.  Usually, this practice
+# is discouraged unless you really know what you're doing (which I do) and
+# this code should not be used in a large scale system.  Therefore, even though
+# adding to the standard classes can make things complicated it makes the code
+# far more expressive.
+#
+
 module SDES
+
+  # Tables used by permutation functions.  These are all predefined,
+  # but not by me.
   
   Initial = %w(2 6 3 1 4 8 5 7)
   
@@ -213,12 +234,22 @@ module SDES
 end
 
 class Fixnum
+  
+  # Converts a number into a byte long binary number.
+  #
+  # Examples:
+  # 15.bin # => 00001111
+  # 6.bin(4) # => 0110
   def bin(padding=8)
     SDES::Utility.dec_to_bin(self, padding)
   end
 end
 
 class Array
+  
+  # Converts an array elements into a string.  If the element is a fixnum
+  # it will convert it into a string first.  This makes changing a permuted
+  # array back into an actual string
   def as_str
     self.map do |i|
       if Fixnum === i
@@ -232,10 +263,18 @@ end
 
 class String
   
+  # Convert a binary number represented as a string and converts it into
+  # a fixnum in base ten.
+  #
+  # Examples:
+  # "1001".dec #=> 9
+  #
   def dec
     SDES::Utility.bin_to_dec(self)
   end
 
+  # This very useful method exists in ruby 1.8.7+ and but if we're running
+  # on 1.8.6 the program won't run at all.
   def each_char
     if block_given?
       scan(/./m) do |x|
