0aab00754cf6309658ecc2525842ccf6f15e629b
Jon Morton <jon.morton@gmail.com>
2010-12-04 15:04:39 -0500

    Simulation outputs both result sets

--- cs465/hw4/simulate.rb
+++ cs465/hw4/simulate.rb
@@ -3,15 +3,8 @@
 # output = %x(dineroIV -informat d -l1-dsize 16K -l1-dbsize 16 -l1-dassoc 2 -l1-drepl l < spice.din)
 # puts output
 
-cache_sizes = %w(2 4 8 16 32)
-block_sizes = %w(16 32 64 128)
-
-def simulate_cache_block(cache_size, block_size)
-  return %x(dineroIV -informat d -l1-usize #{cache_size}K -l1-ubsize #{block_size} < spice.din)
-end
-
 # Matches a string like this:
-#   Demand miss rate	      0.1279	      0.0968	      0.2398	      0.2471	      0.2233	      0.0000
+#   Demand miss rate        0.1279        0.0968        0.2398        0.2471        0.2233        0.0000
 # And returns an array of the column values
 #
 def average_miss_rate(string)
@@ -21,8 +14,8 @@ end
 
 # Reads a string like this:
 #
-# Total Bytes r/w Mem	     2418336
-# ( / Demand Fetches)	      2.4183
+# Total Bytes r/w Mem      2418336
+# ( / Demand Fetches)       2.4183
 #
 # And returns a pair of values 2418336, 2.4183
 #
@@ -31,8 +24,16 @@ def average_memory_traffic(string)
   return matches[0]
 end
 
+def simulate_cache_block(cache_size, block_size)
+  return %x(dineroIV -informat d -l1-usize #{cache_size}K -l1-ubsize #{block_size} < spice.din)
+end
+
 def cache_block_size_metrics
+  cache_sizes = %w(2 4 8 16 32)
+  block_sizes = %w(16 32 64 128)
+
   puts "cache, block, miss rate, traffic"
+
   rs = cache_sizes.map do |cache_size|
     block_sizes.map do |block_size|
       output = simulate_cache_block(cache_size, block_size)
@@ -41,21 +42,31 @@ def cache_block_size_metrics
       result
     end
   end
+  rs
 end
 
 def simulate_replacement_policy(associativity, policy)
   %x(dineroIV -informat d -l1-dsize 16K -l1-dbsize 16 -l1-dassoc #{associativity} -l1-drepl #{policy} < spice.din)
 end
 
-def associativity_replacement_policy
-  rs = %w(r l).map do |policy|
-    %w(1 2 4 8).map do |associativity|
+def associativity_replacement_metrics
+  policies = %w(r l)
+  associativies = %w(1 2 4 8)
+
+  puts "policy, assoc., miss rate, traffic"
+
+  rs = policies.map do |policy|
+    associativies.map do |associativity|
       output = simulate_replacement_policy(associativity, policy)
         result = [ policy, associativity, [ average_miss_rate(output)[0], average_memory_traffic(output)[1] ].flatten ]
         puts result.flatten.join(',')
         result
     end
   end
+  rs
 end
 
-associativity_replacement_policy
+cache_block_size_metrics
+
+associativity_replacement_metrics
+
